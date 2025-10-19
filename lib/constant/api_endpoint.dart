477478a9b582b472f4dbf6bff3c04b7cdccd8ccc
Avatarui1 <65011212186@msu.ac.th>
2025-10-20 00:12:25 +0700

    swap to use firebase srorage to collecting profile

--- lib/constant/api_endpoint.dart
+++ lib/constant/api_endpoint.dart
@@ -1,4 +1,4 @@
 class ApiEndpoints {
   // static const String baseUrl = 'https://project-backend-148h.onrender.com';
-  static const String baseUrl = "https://b2b58e0180a1.ngrok-free.app";
+  static const String baseUrl = "https://a8e5c2f43c68.ngrok-free.app";
 }
