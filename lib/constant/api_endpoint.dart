60971056ad0324ca41b306e2da0abf3a447c5837
Avatarui1 <65011212186@msu.ac.th>
2025-10-20 00:22:23 +0700

    endpoint

--- lib/constant/api_endpoint.dart
+++ lib/constant/api_endpoint.dart
@@ -1,4 +1,3 @@
 class ApiEndpoints {
-  // static const String baseUrl = 'https://project-backend-148h.onrender.com';
-  static const String baseUrl = "https://a8e5c2f43c68.ngrok-free.app";
+  static const String baseUrl = 'https://project-backend-148h.onrender.com';
 }
